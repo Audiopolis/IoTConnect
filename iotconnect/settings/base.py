870c7380ebfe4958607134e30cbc353fdc71edc2
Magnus Bakke <magnusbakke@live.com>
2019-03-04 14:17:59 +0100

    Remove databases and install postgres

--- iotconnect/settings/base.py
+++ iotconnect/settings/base.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
@@ -65,18 +66,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'iotconnect.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
